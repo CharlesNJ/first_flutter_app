b1fd3bcd8853bdb4958c1cbd7bba7718f7b3d7fa
CharlesNJohn <CharlesNJohn@users.noreply.github.com>
2019-07-16 12:28:51 -0500

    Stacks added

--- lib/ui/copy.dart
+++ lib/ui/copy.dart
@@ -5,7 +5,7 @@ class Welcome extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Container(
-        color: Colors.lightGreen,
+        color: Colors.grey,
         alignment: Alignment.center,
         child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -16,6 +16,32 @@ class Welcome extends StatelessWidget {
               new Text('Two',
                   textDirection: TextDirection.ltr,
                   style: new TextStyle(color: Colors.blue)),
+              new Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    const Text('Hello,'),
+                    const Text('there!'),
+                    const Text('How are you today?')
+                  ]),
+        new Stack(
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 90,
+              height: 90,
+              color: Colors.green,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.blue,
+            ),
+          ],
+        ),
               new Container(
                   child: new Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -37,11 +63,9 @@ class Welcome extends StatelessWidget {
                     const Expanded(
                         child: const Text(
                             'HELLO!')) //to occupy the whole rest of the space
-                  ])
+                  ]))
 
-                  //child: new Center(
-
-                  )
+              //child: new Center(
             ]));
   }
 }
