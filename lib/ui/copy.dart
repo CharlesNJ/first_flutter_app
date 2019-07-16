a45fce3e84bdb0001f31358f7dbb5e73648e545c
CharlesNJohn <CharlesNJohn@users.noreply.github.com>
2019-07-15 21:00:37 -0500

    Container and Expanded

--- lib/ui/copy.dart
+++ lib/ui/copy.dart
@@ -1,35 +1,47 @@
 import 'package:flutter/material.dart';
 
-
-
-class Welcome extends StatelessWidget{
+class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Container(
         color: Colors.lightGreen,
-           alignment: Alignment.center,
-           child: new Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: <Widget>[
-               new Text('One', textDirection: TextDirection.ltr,
-               style: new TextStyle(color: Colors.blue)),
+        alignment: Alignment.center,
+        child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('One',
+                  textDirection: TextDirection.ltr,
+                  style: new TextStyle(color: Colors.blue)),
+              new Text('Two',
+                  textDirection: TextDirection.ltr,
+                  style: new TextStyle(color: Colors.blue)),
+              new Container(
+                  child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                    new Text('Hello Church!',
+                        textDirection: TextDirection.ltr,
+                        style: new TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.italic)),
 
-               new Text('Two',textDirection: TextDirection.ltr,
-                 style: new TextStyle(color: Colors.blue)),
+                    new Text('Welcome to ICAT App!',
+                        textDirection: TextDirection.ltr,
+                        style: new TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.italic)),
 
-                new Container(
-                    child: new Text(
-                    'Hello Church!',
-                    textDirection: TextDirection.ltr,
-                    style:new TextStyle(fontSize:50 , fontWeight: FontWeight.w700,fontStyle: FontStyle.italic )))
-                 ]
-           )
+                    const Expanded(
+                        child: const Text(
+                            'HELLO!')) //to occupy the whole rest of the space
+                  ])
 
-        //child: new Center(
+                  //child: new Center(
 
-    );
-
+                  )
+            ]));
   }
-
 }
