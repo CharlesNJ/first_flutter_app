306b8d9490855b640bfb025a9401655ebaa02d38
CharlesNJohn <CharlesNJohn@users.noreply.github.com>
2019-07-15 20:12:38 -0500

    Container in a container

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:first_flutter_app/ui/copy.dart';
 import 'package:flutter/material.dart';
-import './ui/copy.dart';
+
 
 void main() {
   runApp(new MaterialApp(
-    //color: Colors.red,
+    //color: Colors.red, //this doesn't have any affect because the
+    //container overwrites it
     home: new Welcome(),
 
   ));
--- lib/ui/copy.dart
+++ lib/ui/copy.dart
@@ -6,14 +6,30 @@ class Welcome extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Material(
-        color: Colors.red,
-        child: new Center(
-            child: new Text(
-                'Hello Church!',
-                textDirection: TextDirection.ltr,
-                style:new TextStyle(fontSize: 25, fontWeight: FontWeight.w700)))
+    return new Container(
+        color: Colors.lightGreen,
+           alignment: Alignment.center,
+           child: new Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               new Text('One', textDirection: TextDirection.ltr,
+               style: new TextStyle(color: Colors.blue)),
+
+               new Text('Two',textDirection: TextDirection.ltr,
+                 style: new TextStyle(color: Colors.blue)),
+
+                new Container(
+                    child: new Text(
+                    'Hello Church!',
+                    textDirection: TextDirection.ltr,
+                    style:new TextStyle(fontSize:50 , fontWeight: FontWeight.w700,fontStyle: FontStyle.italic )))
+                 ]
+           )
+
+        //child: new Center(
+
     );
+
   }
 
 }
